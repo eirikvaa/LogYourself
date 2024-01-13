be26ac9bb373279040a0e91d5e1a06313294e4a9
Eirik Vale Aase <e.v.aase@gmail.com>
2024-01-13 12:09:27 +0100

    Add more stuff for KMMBridge

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,3 +3,17 @@ plugins {
     alias(libs.plugins.androidLibrary).apply(false)
     alias(libs.plugins.kotlinMultiplatform).apply(false)
 }
+
+val autoVersion = project.property(
+    if (project.hasProperty("AUTO_VERSION")) {
+        "AUTO_VERSION"
+    } else {
+        "LIBRARY_VERSION"
+    }
+) as String
+
+subprojects {
+    val GROUP: String by project
+    group = GROUP
+    version = autoVersion
+}
